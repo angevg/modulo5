1b500becebb80fa987faa4f2ac89b606555ad108
Ange Vg <aavg8@hotmail.com>
2019-12-03 01:52:57 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php
+session_start();
+
+require 'database.php';
+
+if (isset($_SESSION['id_users'])) {
+  $records= $conn->prepare('SELECT id_users, nombre, email, username, password FROM users WHERE id_users=:id_users');
+  $records->bindParam(':id_users', $_SESSION['id_users']);
+  $records->execute();
+  $results = $records->fetch(PDO::FETCH_ASSOC);
+  $user = null;
+
+  if (count($results) > 0) {
+   $user = $results;
+  }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="es">
   <head>
@@ -12,8 +31,18 @@
 <?php
 require 'partials/header.php';
 ?>
+
+<?php  if (!empty($user)): ?>
+  <br>WElcome. <?= $user['nombre'] ?>
+<br>You Are here
+  <a href="Logout.php">Logout</a> or
+
+<?php else: ?>
 	<h2>Login or SignUp</h2>
   <a href="Login.php">Login</a> or <a href="signup.php">SignUp</a>
+  <a href="Login.php">Login</a> or
+   <a href="signup.php">SignUp</a>
+<?php endif; ?>
 
 
 
